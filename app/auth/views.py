4aa1674e39f5306009aebab2874b002f40185c5e
GraceMwende <grace.micheni@student.moringaschool.com>
2022-02-14 22:38:24 +0300

    email on login

--- app/auth/views.py
+++ app/auth/views.py
@@ -15,6 +15,9 @@ def register():
     user = User(email=form.email.data, username=form.username.data, password=form.password.data)
     db.session.add(user)
     db.session.commit()
+
+    mail_message("Welcome to myBlog", 'email/welcome_user', user.email, user=user)
+
     return redirect(url_for('auth.login'))
     title = 'New Account'
 
